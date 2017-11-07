29750435f617c148e620689d862da84729b56ed8
P4py <matthieu.valvin@gmail.com>
2017-11-07 14:53:32 +0100

    ExtractRequestV2

--- ProjetWebSem/src/ExtractHTML.java
+++ ProjetWebSem/src/ExtractHTML.java
@@ -1,60 +1,44 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class ExtractHTML {
 
-	public static final String CACHE_DIRECTORY = "cache/";
-	
-	public static void main(String[] args) throws IOException {
-		String key = "AIzaSyBspyc1fD-X92bkL7RdKoiI55c2n3Hr_vE";
-		String qry="Toz";
+	private static final String CACHE_DIRECTORY = "cache/";
+
+	public static void main(String args[])
+	{
+		String qry = "";
+
+		extractURLFromRequest(qry);
+	}
+	public static ArrayList<String> extractURLFromRequest(String qry){
 
 		ArrayList<String> list = new ArrayList<String>();
 
 		File cacheFile = new File(CACHE_DIRECTORY + qry +".txt");
 		if(!cacheFile.exists()) {
-
-			URL url = new URL(
-					"https://www.googleapis.com/customsearch/v1?key="+key+ "&cx=013036536707430787589:_pqjad5hr1a&q="+ qry + "&alt=json");
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("Accept", "application/json");
-			BufferedReader br = new BufferedReader(new InputStreamReader(
-					(conn.getInputStream())));
-
-			String output;
-			System.out.println("Output from Server .... \n");
-			
-			PrintWriter writer = new PrintWriter(CACHE_DIRECTORY + qry+".txt", "UTF-8");
-			
-			while ((output = br.readLine()) != null) {
-				if(output.contains("\"link\": \"")){                
-					String link=output.substring(output.indexOf("\"link\": \"")+("\"link\": \"").length(), output.indexOf("\","));
-					System.out.println(link);       //Will print the google search links
-					list.add(link);
-					writer.println(link);
-				}     
-			}
-			writer.close();
-			conn.disconnect();
+			GoogleRequest gr = new GoogleRequest();
+			list = gr.getRequestResult(qry);
 		} else {
-			BufferedReader reader = null;
-			reader = new BufferedReader(new FileReader(CACHE_DIRECTORY + qry + ".txt"));
-			
-			StringBuilder sb = new StringBuilder();
-			String line = reader.readLine();
-			
-			while( line != null ) {
-				System.out.println(line);
+			try {
+				System.out.println("Output from cache file .... \n");
+				BufferedReader reader = null;
+				reader = new BufferedReader(new FileReader(CACHE_DIRECTORY + qry + ".txt"));
+
+				String line = reader.readLine();
+
+				while( line != null ) {
+					System.out.println(line);
+					list.add(line);
+					line = reader.readLine();
+				}
+				reader.close();
+			} catch( Exception e) {
+				e.printStackTrace();
 			}
 		}
-	}   
+		return list;
+	}
 }
