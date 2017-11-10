6d580789ee50d540453ed38dbe3cfb981bf5d2d6
baptiste <baptiste.dubarry21@gmail.com>
2017-11-10 11:28:23 +0100

    Spotlight is working

--- ProjetWebSem/src/Spotlight.java
+++ ProjetWebSem/src/Spotlight.java
@@ -10,21 +10,32 @@ import javax.swing.plaf.synth.SynthSeparatorUI;
 import org.json.*;
 
 public class Spotlight {
-	public static JSONObject SpotlightText (String text) throws IOException {
-		String url = "http://model.dbpedia-spotlight.org/fr/annotate";
+	public static JSONArray SpotlightText (String text) throws IOException {
+		
+		final String url = "http://model.dbpedia-spotlight.org/fr/annotate";
+		
+		String conf = "0.3";
+		
 		HTTPRequest httpReq = new HTTPRequest(url);
 		httpReq.setHeader("Accept", "application/json");
 		httpReq.addParam("text", URLEncoder.encode(text, "utf-8"));
-		httpReq.addParam("confidence", "0.1");
+		httpReq.addParam("confidence", conf);
+		
 		String res = httpReq.doRequest();
-		System.out.println(res);
+		
 		JSONObject json = new JSONObject(res);
-		System.out.println(json);
-		return json;
+		JSONArray test = (JSONArray) json.get("Resources");
+		JSONArray URIs = new JSONArray();
+		
+		for(int i =0; i<test.length(); i++) {
+			JSONObject tmp = test.getJSONObject(i);
+			URIs.put(tmp.get("@URI"));
+		}
+			return URIs;
 	}
 		public static void main(String[] args) throws IOException {
-			JSONObject jsontest;
-			jsontest = SpotlightText("Donal Trump Barrack Obama George Bush");
-		
+			String text = "La ville de Berlin se situe dans le nord-est de l'Allemagne, dans la plaine germano-polonaise, à 33 m d'altitude, au confluent de la Spree et de la Havel. Une particularité de la ville est la présence de nombreux lacs et rivières, le long des cours d'eau. On en trouve plusieurs à l'ouest, mais aussi à l'est avec le Müggelsee. Berlin est égayée par plusieurs rivières, canaux, parcs et lacs (Havel, Wannsee, Müggelsee, Spree, Dahme, Landwehrkanal). Elle possède en outre une architecture ancienne et classique très riche.";
+			JSONArray jsontest = SpotlightText(text);
+			System.out.println(jsontest);
 		}
 }
