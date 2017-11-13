9217dff5a4835d2ccbf1ff7a73645a3a8531e22c
baptiste <baptiste.dubarry21@gmail.com>
2017-11-13 17:41:25 +0100

    Start working on Main, put getRequest static

--- ProjetWebSem/src/Main.java
+++ ProjetWebSem/src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir votre requête :");
+		String query =  sc.nextLine();
+		sc.close();
+		ArrayList<String> urls = GoogleRequest.getRequestResult(query);
 
 	}
 
